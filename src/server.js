0c44edcdcc3b60069920e2e3247d25b8c0596972
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2026-03-09 19:36:55 -0300

    Feat: Ajusta os arquivos relacionados ao banco

--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
 
 app.get("/", (req, res) => {
   res.send("🚀 API funcionando");
